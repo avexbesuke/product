220d4ddcd0e0acbadfb6cd07ac4e3683e0e8334c
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-28 12:51:38 +0900

    devise_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy" 
