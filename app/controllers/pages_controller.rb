1d999adf955b062bb7947cd32c98887f5d99c0dc
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-29 20:20:12 +0900

    mypage_date_create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @readed_books = @user.read_books.limit(3)
+    @fast_best_book = @user.books.first
+    @fast_best_emotion = @user.emotions.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  root 'pages#about'
+
+  resources :pages , only: [:show,:index]
 
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
