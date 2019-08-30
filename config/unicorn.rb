d8106bb1d07e977a3b7e55b0e0d0d73ee4f8e08f
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-30 19:21:06 +0900

    unicorn_config_change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,28 @@
-app_path = File.expand_path('../../../', __FILE__)
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../', __FILE__)
 
+#アプリケーションサーバの性能を決定する
 worker_processes 1
 
-working_directory "#{app_path}/current"
+#アプリケーションの設置されているディレクトリを指定
+working_directory app_path
 
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
+#ポート番号を指定
+listen 3000
+
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+#Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
+
+#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
@@ -23,7 +36,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false
+    run_once = false # prevent from firing again
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
