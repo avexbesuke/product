d1c28d010644c56ab76cf5599f57c722f87e1703
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-30 15:28:07 +0900

    user_image_craete

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship",foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
 
+  has_one_attached :image
+
   def user_mybest?(book)
     current_user.book
   end
