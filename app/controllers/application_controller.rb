333039b4488b9305ece27e473804bab6414a794e
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-31 00:48:11 +0900

    err_refact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
     new_user_session_path
