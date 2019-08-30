52b2a21c8bb98632b35957fef58e643bf760936d
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-30 11:42:09 +0900

    follow_action_create

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,12 @@
 class RelationshipsController < ApplicationController
   def create
+    user = User.find(current_user.id)
+    if User.find(params[:user_id]).followed_by?(current_user)
+      follow_cancel = current_user.active_relationships.find_by(follower_id: params[:user_id])
+      follow_cancel.destroy
+    else
+      follow_do = current_user.active_relationships.new(follower_id: params[:user_id])
+      follow_do.save
+    end
   end
 end
