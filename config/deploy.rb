4348acf16590c754fede20f3aeee453722908f74
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-02 16:55:36 +0900

    S3_production_setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,10 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/master.key }
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
