dc28cd2e2b8d8b35d0ac7964b7dc5eae4e8cf581
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-02 14:12:07 +0900

    layout_edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
    #If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:profile,:image])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:username,:email,:profile,:image])
    end
 
    #The path used after sign up.
