a8dce869d6a115129be1d6c25ed6cbbc3628b203
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-02 09:57:58 +0900

    unicorn.rb_edit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,8 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
-
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
 # currentを指定
 working_directory "#{app_path}/current"
 
