3f966b345a3f7b28daec19093f4e38c67a433c3d
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-05 20:21:12 +0900

    multitipe_true

--- app/models/map.rb
+++ app/models/map.rb
@@ -6,4 +6,21 @@ class Map < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
+
+  validate :file_type
+  validate :image_size
+
+  private
+
+  def image_size
+    if images.each do |image|
+      errors.add(:images, '1メガバイト以上は添付できません。') if image.blob.byte_size > 1.megabytes end
+    end
+  end
+
+  def file_type
+    if images.each do |image|
+      errors.add(:image, 'needs to be a JPEG or PNG') unless image.content_type.in?(%('image/jpeg image/png'))end
+    end
+  end
 end
