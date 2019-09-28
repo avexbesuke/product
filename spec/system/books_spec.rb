017d0a6d86585571b19ba04c7a30cf1908cf4303
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-28 20:44:11 +0900

    key: v2-dependencies-{{ checksum "app" }}

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -34,59 +34,6 @@ describe '本の機能', type: :system do
         expect(page).to have_content '星の王子さま'
       end
     end
-
-    context '本をマイベストにした時' do
-      before do
-        fill_in 'keyword', with: '星の王子'
-        page.first(".el-button").click
-
-        page.first(".best-btn").click
-        fill_in 'emotion_body', with: '好きな本です'
-        fill_in 'emotion_tag_list', with: '感動,笑える'
-        click_on "登録"
-      end
-
-      it 'マイベスト一覧に理由が表示される' do
-        visit emotions_path
-        expect(page).to have_content '好きな本です'
-      end
-
-      it 'マイベスト一覧にタグが表示される' do
-        page.first(".fa-commenting").click
-        expect(page).to have_content '感動'
-      end
-
-      it 'マイベスト一覧から検索が出来る' do
-        page.first(".fa-commenting").click
-        fill_in 'q_body_or_book_title_cont', with: '好きな本です'
-        page.first(".fa-search").click
-        expect(page).to have_content '好きな本です'
-      end
-
-      it 'マイページに理由が表示される' do
-        page.first(".fa-bookmark").click
-        expect(page).to have_content '好きな本です'
-      end
-
-      it 'マイページにタグが表示される' do
-        page.first(".fa-bookmark").click
-        expect(page).to have_content '感動'
-      end
-
-      it '編集した時に本文が変わる' do
-        page.first(".fa-bookmark").click
-        page.first(".fa-edit").click
-        fill_in 'emotion_body', with: '良き本です'
-        click_on "決定"
-        page.first(".fa-bookmark").click
-        expect(page).to have_content '良き本です'
-      end
-
-      it '削除した時に表示されない' do
-        page.first(".fa-bookmark").click
-        page.first(".fa-trash").click
-        expect(page).not_to have_content '良き本です'
-      end
-    end
+    
   end
 end
