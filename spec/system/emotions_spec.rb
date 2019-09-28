2cdf137c51b4eafae2bb675b03e19861c349663b
avexbesuke <xyezsbf3@i.softbank.jp>
2019-09-28 21:29:37 +0900

    retry

--- spec/system/emotions_spec.rb
+++ spec/system/emotions_spec.rb
@@ -7,6 +7,7 @@ describe '本を好きな理由投稿機能', type: :system do
       FactoryBot.build(:book)
 
       visit new_user_session_path
+      visit current_path
       fill_in 'user[email]', with: 'test@test.com'
       fill_in 'user[password]', with: 'testpass'
       click_button "ログイン"
