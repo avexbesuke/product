827fdce5d673bc2a37873390cb3465d50b990b25
avexbesuke <xyezsbf3@i.softbank.jp>
2019-10-05 09:46:10 +0900

    rout追記

--- app/javascript/packs/router/router.js
+++ app/javascript/packs/router/router.js
@@ -10,6 +10,12 @@ export default new Router({
   base: process.env.BASE_URL,
   routes:[
     {path: '/', name: 'search', component: BookSearch},
+    {path: '/maps', name: 'search', component: BookSearch},
+    {path: '/pages/*', name: 'search', component: BookSearch},
+    {path: '/emotions', name: 'search', component: BookSearch},
+    {path: '/reads', name: 'search', component: BookSearch},
+    {path: '/books/*', name: 'search', component: BookSearch},
+    {path: '/users/*', name: 'search', component: BookSearch},
     {path: '/form', name: 'form', component: BookForm},
     {path: '*', redirect: '/'}
   ],
